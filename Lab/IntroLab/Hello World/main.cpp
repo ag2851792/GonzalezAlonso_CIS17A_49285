d61aa85d31325ed70aef05bce265db5c849be4f1
ag2851792 <agonzalez621@student.rccd.edu>
2020-09-01 12:28:31 -0700

    Signed-off-by: ag2851792 <agonzalez621@student.rccd.edu>

---
+++ Lab/IntroLab/Hello World/main.cpp
@@ -0,0 +1,18 @@
+
+/* 
+ * File:   main.cpp
+ * Author: Alonso Gonzalez 
+ * Created on August 31, 2020, 11:29 AM
+ * Purpose: Output Hello World
+ */
+
+//System Libraries 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout << "Hello World!!" << endl;;
+    return 0;
+}
+
